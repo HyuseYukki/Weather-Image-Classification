ab0e6d29b6f855945b1b59926d9b7ad23db7d732
Llen <allen.opena55@gmail.com>
2024-05-19 10:30:08 +0800

    Update App.py

--- App.py
+++ App.py
@@ -30,6 +30,7 @@ def import_and_predict(image_data, model):
     return prediction
 
 if file is None:
+    st.text("Please upload an image file")
 else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
